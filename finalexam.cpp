685048cfcaaca1e9cc45c06138480ad6488053f8
JD Ojeda <jddedios.ojeda@gmail.com>
2024-12-05 13:52:15 -0500

    renamed to fit the criteria of the first quiz

--- finalexam.cpp
+++ finalexam.cpp
@@ -1,9 +1,14 @@
 /**
- * Program used to prepare for the final exam at OSU for CSE 1222
+ * Program used to prepare for the final exam at OSU for CSE 1222.
+ * Exam will have (10ish) -- MC, matching, Drag and drop questions.
+ * There will also be coding questions.
+ * Have all tabs open prior to beginning the exam so you dont get the boot.
+ * 
  */
 
 #include <string>
 #include <vector>
+#include <iostream>
 #include <fstream>
 using namespace std;
 
